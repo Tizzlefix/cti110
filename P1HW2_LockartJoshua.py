b8c7f730bc80c24be712d41e2674084e36afba31
Tizzlefix <99516760+Tizzlefix@users.noreply.github.com>
2022-02-14 12:00:32 -0500

    Add files via upload
    PIZZA PARTY

---
+++ P1HW2_LockartJoshua.py
@@ -0,0 +1,20 @@
+# Pizza party for the students
+    # 2/14/2022
+    # CTI-110 P1HW2 - Pizza Order
+    # Joshua Lockart
+    #
+students = int(input())
+slices = (students * 3)
+pizzas = (students / 2)
+print('How many students do you want pizza for?', students)
+print('----Pizza Order--------')
+print('Number of Students:', students)
+print('Pizza Slices Needed:', slices)
+print('Pizzas Needed:', pizzas)
+print('--------------------------')
+
+    # Pseudocode though I'm not entirely sure if I'm writing it correctly but am giving it a shot
+    # students <-- User input
+    # slices  <-- Takes user input from students and multiplies by 3 to give the number of slices needed
+    # pizzas <-- Takes user input from students and divides by 2 to give the number of pizzas needed (6 slices to 1 pizza)
+    # If user inputs an integer into the variable students then it there will be an OUTPUT

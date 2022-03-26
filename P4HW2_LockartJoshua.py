3429f4b5645ecd551ba234255e978c916aa1fa59
Tizzlefix <99516760+Tizzlefix@users.noreply.github.com>
2022-03-25 22:34:13 -0400

    Add files via upload
    Pizza order cleaned up with a While loop.

---
+++ P4HW2_LockartJoshua.py
@@ -0,0 +1,44 @@
+import math
+# CTI-110
+# P3HW2 - Pizza Order
+# Josh Lockart
+# 3/25/2022
+
+def main():
+
+    while True:
+        # User inputs how many students and people per pizza
+        student_pizza = int(input('How many students do you want pizza for?'))
+        people_per_pizza = float(input('Enter number of people per pizza (1.5, 2, 3):'))
+
+        # Calculating pizzas needed plus the total cost with a 6% tax
+        pizzas_needed = int(student_pizza / people_per_pizza)
+        pizza_tax = float(5 * pizzas_needed * 0.06)
+        pizza_total = float(5 * pizzas_needed + pizza_tax)
+
+        # Outputting user input
+        print('How many students do you want pizza for?', student_pizza)
+        print('Enter number of people per pizza (1.5, 2, 3):', people_per_pizza)
+
+        # Running user input through statements 
+        if people_per_pizza == 1.5 or people_per_pizza == 2 or people_per_pizza == 3:
+            print('----Pizza Order--------')
+            print('Number of Students :', student_pizza)
+            print('Pizzas Needed:', pizzas_needed)
+            print('Price ${:.2f}'.format(pizza_total))
+            print('--------------------------')
+            print('Would you like to run program again (y for yes):')
+            print(input('Would you like to run program again (y for yes):'))
+            
+            
+            
+        else:
+            print()
+            print('INVALD ENTRY!!!!')
+            print('Should have entered, 1.5, 2, or 3\n')
+            print('Enter number of people per pizza again. (1.5, 2, or 3):')
+            
+        
+
+# Executing program
+main()
